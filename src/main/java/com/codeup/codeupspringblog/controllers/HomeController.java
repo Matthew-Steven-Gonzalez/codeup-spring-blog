026f1c82d9dad52fbe8b790ef7a86d1bec2e84cf
Matthew <gonzalez.matthew.steven@gmail.com>
2023-02-22 11:40:09 -0500

    lec

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,14 +1,13 @@
-package com.codeup.codeupspringblog;
+package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello(){
-        return "hello, World!";
+public class HomeController {
+    @GetMapping("/")
+    public String Home(){
+        return "index";
     }
 }
